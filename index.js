aead78b28ce5a9e3d9d335e177ea907f67fb224b
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-12 19:08:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+//Challenge Rewrite the function using '?' or '||'
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Did parents allow you?');
+  }
+}
+
+// checkAge(17)
+
+//Solution 1 with ?
+function checkAge(age) {
+  return (age > 18) ? true : confirm('Did parents allow you?')
+}
+
+//Solution 2 with ||
+
+function checkAge(age) {
+  return (age > 18) || confirm('Did parents allow you?')
+}
+
+// Challenge Function min(a, b)
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+//Solution 1 with If Else Statement or Ternary
+function min(a, b) {
+return (a < b) ? a : b;
+}
+
+console.log(min(4,3))
+
+//Solution 2 with built in function Math.min
+function min2(a, b) {
+return Math.min(a,b)
+}
+
+console.log(min2(82.222,12.1133))
+
+//Challenge Function pow(x,n)
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result. 
+//pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+
+//Solution 1 with For Loop
+function pow(x,n) {
+  let result = x
+  for (let i = 1; i < n; i++) {
+    result *= x
+  }
+return result
+}
+
+console.log(pow(3,2))
+
+//Solution 2 with built-in function Math.pow
+function pow2(a,b) {
+  return Math.pow(a,b)
+}
+
+console.log(pow2(3.3,3))
